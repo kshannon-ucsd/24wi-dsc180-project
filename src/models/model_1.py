5aa66324239c695a77f8de394dafe304864a888d
rduvur <rohan.duvur@gmail.com>
2025-03-07 02:16:36 +0000

    slight model improvements

--- src/models/model_1.py
+++ src/models/model_1.py
@@ -8,10 +8,18 @@ def build_model():
     resnet50 = ResNet50V2(include_top=False, weights='imagenet', pooling='max', input_shape=(224, 224, 3))
     model = Sequential([
         resnet50,
-        Dense(256), BatchNormalization(), Activation('relu'), Dropout(0.5),
-        Dense(128), Activation('relu'),
+        Dense(512, kernel_regularizer=l2(5e-4)),
+        BatchNormalization(),
+        Activation('relu'),
+        Dropout(0.6),  
+        Dense(256, kernel_regularizer=l2(5e-4)),
+        BatchNormalization(),
+        Activation('relu'),
+        Dropout(0.5),
+        Dense(128, kernel_regularizer=l2(5e-4)),
+        Activation('relu'),
         Dense(1, activation="sigmoid")
-    ])
+        ])
     lr_schedule = CosineDecayRestarts(initial_learning_rate=0.0001, first_decay_steps=5*326, t_mul=2.0, m_mul=0.8, alpha=1e-6)
     model.compile(optimizer=Adam(learning_rate=lr_schedule), loss='binary_crossentropy', metrics=['accuracy', 'AUC'])
     return model
